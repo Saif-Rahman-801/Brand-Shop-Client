7957bed09f3f2b3fb653ec04bd452cc1ef9427b5
Saif-Rahman-801 <saifurs102@gmail.com>
2023-10-20 21:27:02 +0600

    Login  form created

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,25 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Register = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+  };
+
   return (
     <div>
       <div className="bg-gray-100 h-screen flex items-center justify-center">
         <div className="bg-white p-8 rounded shadow-md max-w-md w-full">
-          <h2 className="text-2xl font-semibold mb-6">Registration Form</h2>
-          <form>
-            <div className="mb-4">
-              <label
-                htmlFor="name"
-                className="block text-sm font-medium text-gray-600"
-              >
-                Name
-              </label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                className="mt-1 p-2 block w-full border rounded-md focus:outline-none focus:ring focus:border-blue-300"
-              />
-            </div>
-
+          <h2 className="text-2xl font-semibold mb-6">Please Register </h2>
+          <form onSubmit={handleSubmit}>
             <div className="mb-4">
               <label
                 htmlFor="email"
@@ -56,7 +50,16 @@ const Register = () => {
             >
               Register
             </button>
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300 my-3"
+            >
+              Register With Google
+            </button>
           </form>
+          <p>
+            Already have an account? <Link to="/login">Please Login </Link>
+          </p>
         </div>
       </div>
     </div>
